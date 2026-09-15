815f246e70aa4e53805b60629d874a9f8d11bac5
10072008duda <s.jeniffer10@escola.pr.gov.br>
2026-09-15 13:15:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -323,7 +323,49 @@ const textoResultado = document.querySelector(“.texto-resultado”);const perg
         texto: "Isso é maravilhoso!",
         afirmacao: [
           "Quis saber como usar IA no seu dia a dia.",
-          "Foi atrás de vídeos, artigos e mais informaçõe sobre como utilizar essa tecnologia.",
+          "Fofunction aleatorio (lista){
+        const posicao = Math.floor(Math.random()* lista.length);
+        return lista[posicao];
+}function respostaSelecionada(opcaoSelecionada){
+        const afirmacoes = opcaoSelecionada.afirmacao;
+        historiaFinal += afirmacoes + " ";
+        atual++;
+        mostraPergunta();
+}function respostaSelecionada(opcaoSelecionada){
+        const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
+        historiaFinal += afirmacoes + " ";
+        atual++;
+        mostraPergunta();
+}function aleatorio(lista) {
+
+}function aleatorio(lista) {
+    const posicao = Math.random()* lista.length;
+}function aleatorio(lista) {
+const posicao = Math.floor(Math.random()* lista.length);
+}function aleatorio(lista) {
+const posicao = Math.floor(Math.random()* lista.length);
+return lista[posicao];
+}function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+historiaFinal += afirmacoes + “ “;
+atual++;
+mostraPergunta();
+}function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
+historiaFinal += afirmacoes + “ “;
+atual++;
+mostraPergunta();
+}function dadoD6() {
+    var numero = Math.floor(Math.random() * 6);
+    return numero;
+}
+
+console.log(dadoD6());function dadoD6() {
+    var numero = Math.floor(Math.random() * 6) + 1;
+    return numero;
+}
+
+console.log(dadoD6());i atrás de vídeos, artigos e mais informaçõe sobre como utilizar essa tecnologia.",
         ],
       },
     ],
@@ -345,4 +387,6 @@ function unidadeEscolhida(numero){
 return unidades[numero];
 }
 
-console.log(unidadeEscolhida(0));
+console.log(unidadeEscolhida(0));function aleatorio(lista) {
+        const posicao = Math.random()* lista.length;
+}
